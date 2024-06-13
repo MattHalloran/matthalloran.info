0579cf24b231b1a4436ceafe9b1dff482e857245
MattHalloran <matthalloran8@gmail.com>
2024-06-13 15:14:57 -0400

    More crypto donation options
    - Added more crypto donation options
    - Improved the look of the donation dialog

--- src/components/DonateDialog/DonateDialog.tsx
+++ src/components/DonateDialog/DonateDialog.tsx
@@ -1,84 +1,117 @@
-import { ContentCopy as CopyIcon } from "@mui/icons-material";
-import { Box, Button, Stack, Typography } from "@mui/material";
-import QRCode from "assets/qr-60pc.png";
+import { Box, Button, IconButton, Stack, Typography } from "@mui/material";
+import { CloseIcon, CopyIcon } from "assets/icons";
+import QRCode from "assets/qr-ada.png";
 import { LargeDialog } from "components/LargeDialog/LargeDialog";
 import { useCallback, useMemo, useState } from "react";
 import { buttonProps, noSelect } from "styles";
 import { DonateDialogProps } from "../types";
 
-const address = "addr1qxmd7082f6nrx4468lya90208pnr8ahr770amfch7alm6584rksxwqykx6y2kxtaavcnasskw97k3qhlnffs46sgvqvqr060pc";
-const addressDisplay = "addr1...qr060pc";
-const handle = "$vrooli";
+type CryptoInfo = {
+    address?: string;
+    addressDisplay?: string;
+    handle?: string;
+    symbol?: string;
+    qrCode?: string;
+};
+
+const cryptoInfo: Record<string, CryptoInfo> = {
+    BTC: {
+        address: "bc1qlv6sz7xcd8dpzmklnmlls6nazynx44ymhfrwhl",
+        addressDisplay: "bc1q...whl",
+        symbol: "₿",
+    },
+    ADA: {
+        address: "addr1qxmd7082f6nrx4468lya90208pnr8ahr770amfch7alm6584rksxwqykx6y2kxtaavcnasskw97k3qhlnffs46sgvqvqr060pc",
+        addressDisplay: "addr1...qr060pc",
+        handle: "$vrooli",
+        symbol: "₳",
+        qrCode: QRCode,
+    },
+    ETH: {
+        address: "0xBba1D847961d0e0B58BC557D329a4B7DEeD81345",
+        addressDisplay: "0xBba1...1345",
+        symbol: "Ξ",
+    },
+    SOL: {
+        address: "BL7DXV3YPFd7tjzKtTQxmvorCWfGwjfARKP274nbSRH3",
+        addressDisplay: "BL7D...SRH3",
+        symbol: "◎",
+    },
+    Coinbase: {
+        handle: "mdhalloran.cb.id",
+    },
+};
 
 export const DonateDialog = ({
     open,
     onClose,
 }: DonateDialogProps) => {
-    const [adaOptionSelected, setAdaOptionSelected] = useState(false);
+    const [selectedCrypto, setSelectedCrypto] = useState<string | null>(null);
     const [copied, setCopied] = useState<boolean>(false);
 
     const openLink = useCallback((link: string) => { window.open(link, "_blank", "noopener,noreferrer"); }, []);
 
     const handleClose = useCallback(() => {
-        setAdaOptionSelected(false);
+        setSelectedCrypto(null);
         setCopied(false);
         onClose();
     }, [onClose]);
 
-    const copyHandle = () => {
-        navigator.clipboard.writeText(handle);
+    const copyToClipboard = useCallback((text: string) => {
+        navigator.clipboard.writeText(text);
         setCopied(true);
         setTimeout(() => setCopied(false), 5000);
-    };
-
-    const copyAddress = () => {
-        navigator.clipboard.writeText(address);
-        setCopied(true);
-        setTimeout(() => setCopied(false), 5000);
-    };
+    }, []);
 
     const mainOptions = useMemo(() => (
         <>
-            <Typography id='modal-title' variant="h4" component="h2" mb={2}>Support the Project</Typography>
-            <Stack direction="column" spacing={1} mb={2} sx={{ ...noSelect, alignItems: "center" }}>
-                <Typography variant="body1" component="h3" mb={2}>The development of Vrooli is mostly funded my life's savings. Would you be so kind as to help fund the project before it becomes sustainable?</Typography>
-                <Button onClick={() => openLink("https://ko-fi.com/matthalloran")} sx={{ ...buttonProps }}>Donate - Paypal/Stripe</Button>
-                <Button onClick={() => setAdaOptionSelected(true)} sx={{ ...buttonProps }}>Donate - ₳</Button>
+            <Typography id='modal-title' variant="h4" component="h2" mb={2}>Support Me?👉👈</Typography>
+            <Typography variant="body1" component="h3">Building a utopia is my full-time job.</Typography>
+            <Typography variant="body1" component="h3">Can you help me fund Vrooli before it becomes sustainable?</Typography>
+            <Stack direction="column" spacing={1} mt={2} mb={2} sx={{ ...noSelect, alignItems: "center" }}>
+                <Button onClick={() => openLink("https://ko-fi.com/matthalloran")} sx={{ ...buttonProps }}>USD ($)</Button>
+                {Object.entries(cryptoInfo).map(([key, { symbol }]) => (
+                    <Button key={key} onClick={() => setSelectedCrypto(key)} sx={{ ...buttonProps }}>
+                        {symbol ? `${key} (${cryptoInfo[key].symbol})` : key}
+                    </Button>
+                ))}
             </Stack>
         </>
-    ), [setAdaOptionSelected, openLink]);
+    ), []);
 
-    const adaDonateOptions = useMemo(() => (
-        <>
-            <Box mb={1} sx={{
-                padding: "10px",
-            }}>
-                <img
-                    src={QRCode} alt="Donate ₳ QR code"
-                    style={{
-                        height: "225px",
-                        border: "1px solid black",
-                    }}
-                />
-            </Box>
-            <Stack direction="column" spacing={2} mb={2} sx={{ alignItems: "center" }}>
-                <Typography id='modal-title' variant="h4" component="h2">Donate ₳? 🥺👉👈</Typography>
-                <Typography variant="body1">1. Scan QR code or press address/handle to copy</Typography>
-                <Typography variant="body1">2. Send ADA to address/handle with your wallet</Typography>
-                <Button
-                    onClick={copyHandle}
-                    startIcon={<CopyIcon />}
-                    sx={{ ...buttonProps }}
-                >Handle - {handle}</Button>
-                <Button
-                    onClick={copyAddress}
-                    startIcon={<CopyIcon />}
-                    sx={{ ...buttonProps }}
-                >Address - {addressDisplay}</Button>
-            </Stack>
-            {copied ? <Typography variant="h6" component="h4" textAlign="center" mb={1}>🎉 Copied! 🎉</Typography> : null}
-        </>
-    ), [copied]);
+    const selectedCryptoOptions = useMemo(() => {
+        if (!selectedCrypto) return null;
+        const { address, addressDisplay, handle, qrCode } = cryptoInfo[selectedCrypto] || {};
+
+        return (
+            <>
+                {qrCode && (
+                    <>
+                        <Box mb={2} sx={{ borderRadius: 2 }}>
+                            <img src={qrCode} alt={`${selectedCrypto} QR code`} style={{ height: "225px", border: "1px solid black" }} />
+                        </Box>
+                        <Stack direction="column" spacing={1} mt={2} mb={4} sx={{ alignItems: "center" }}>
+                            <Typography variant="body1">1. Scan QR code or press address/handle to copy</Typography>
+                            <Typography variant="body1">2. Send {selectedCrypto} to address/handle with your wallet</Typography>
+                        </Stack>
+                    </>
+                )}
+                <Stack direction="column" spacing={2} mt={qrCode ? 4 : 0} mb={2} sx={{ alignItems: "center" }}>
+                    {handle && (
+                        <Button onClick={() => copyToClipboard(handle)} startIcon={<CopyIcon />} sx={{ ...buttonProps }}>
+                            Handle - {handle}
+                        </Button>
+                    )}
+                    {address && (
+                        <Button onClick={() => copyToClipboard(address)} startIcon={<CopyIcon />} sx={{ ...buttonProps }}>
+                            Address - {addressDisplay}
+                        </Button>
+                    )}
+                </Stack>
+                {copied ? <Typography variant="h6" component="h4" textAlign="center" mb={1}>🎉 Copied! 🎉</Typography> : null}
+            </>
+        );
+    }, [selectedCrypto, copied, copyToClipboard]);
 
     return (
         <LargeDialog
@@ -87,13 +120,21 @@ export const DonateDialog = ({
             onClose={handleClose}
             sxs={{
                 paper: {
-                    background: (copied && adaOptionSelected) ? "#1c7602" : "#1e3558",
+                    background: (copied && selectedCrypto) ? "#1c7602" : "#1e3558",
                     transition: "background 0.2s ease-in-out",
                 },
             }}
         >
+            <Box sx={{ width: "100%", display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
+                <IconButton
+                    onClick={handleClose}
+                    sx={{ paddingTop: 1, paddingRight: 2 }}
+                >
+                    <CloseIcon fill="white" width={36} height={36} />
+                </IconButton>
+            </Box>
             <Box p={2} sx={{ textAlign: "center" }}>
-                {adaOptionSelected ? adaDonateOptions : mainOptions}
+                {selectedCrypto ? selectedCryptoOptions : mainOptions}
             </Box>
         </LargeDialog>
     );
